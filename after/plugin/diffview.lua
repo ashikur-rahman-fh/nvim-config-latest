f6e951488c915021c457b3f1f8352f348a813a16
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-07-16 21:23:38 -0400

    Add diffview git history

--- after/plugin/diffview.lua
+++ after/plugin/diffview.lua
@@ -3,3 +3,5 @@ vim.keymap.set('n', '<leader>do', ':DiffviewOpen<CR>', { desc = 'Open Diffview' 
 vim.keymap.set('n', '<leader>dc', ':DiffviewClose<CR>', { desc = 'Close Diffview' })
 
 vim.keymap.set('n', '<leader>dh', ':DiffviewFileHistory %<CR>', { desc = 'Diffview File History (current file)' })
+
+vim.keymap.set('n', '<leader>gh', ':DiffviewFileHistory<CR>', { desc = 'Diffview git commit history' })
