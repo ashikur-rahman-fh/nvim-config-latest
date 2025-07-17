ae9530eab676c21ac04171fca5f97752ed99f794
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-07-16 20:16:00 -0400

    Add telescope extension fzf for performance

--- lua/ash/packer.lua
+++ lua/ash/packer.lua
@@ -10,7 +10,10 @@ return require('packer').startup(function(use)
   use {
     'nvim-telescope/telescope.nvim',
     tag = '0.1.8',
-    requires = { {'nvim-lua/plenary.nvim'} }
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    }
   }
   use { 'mbbill/undotree' }
   use {
