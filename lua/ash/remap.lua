35d738644b3bd169bd55e5dfdf6251e95f905f15
Ashikur Rahman <ashikurrahmanvar@gmail.com>
2025-07-23 10:40:25 -0400

    Add remap to system copy/paste

--- lua/ash/remap.lua
+++ lua/ash/remap.lua
@@ -1,2 +1,7 @@
 vim.g.mapleader = ' '
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
+-- Copy/Paste
+vim.keymap.set("v", "<C-c>", '"+y', { noremap = true, silent = true })
+vim.keymap.set({ "n", "v" }, "<C-v>", '"+p', { noremap = true, silent = true })
+vim.keymap.set("i", "<C-v>", '<C-r>+', { noremap = true, silent = true })
